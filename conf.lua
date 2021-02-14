337dcd5d16ce723ae112d6cad4137eb9700d27c5
Joseantonio920 <71590697+Joseantonio920@users.noreply.github.com>
2021-02-14 12:21:05 -0300

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,47 @@
+function love.conf(t)
+	t.identity=nil
+	t.appendidentity=false
+	t.version="11.3"
+	t.console=false
+	t.accelerometerjoystick=false
+	t.externalstorage=false
+	t.gammacorrect=false
+	t.audio.mic=false
+	t.audio.mixwithsystem=false
+	t.window.title="jogo da turma"
+	t.window.icon=nil
+	t.window.width=512
+	t.window.height=256
+	t.window.borderless=true
+	t.window.resizable=false
+	t.window.fullscreen=false
+	t.window.fullscreentype="desktop"
+	t.window.vsync=1
+	t.window.msaa=0
+	t.window.depth=nil
+	t.window.stencil=8
+	t.window.display=nil
+	t.window.highdpi=false
+	t.window.usedpiscale=true
+	t.window.x=nil
+	t.window.y=nil
+	
+	t.modules.audio=false
+	t.modules.data=true
+	t.modules.event=true
+	t.modules.font=true
+	t.modules.graphics=true
+	t.modules.image=true
+	t.modules.joystick=false
+	t.modules.keyboard=true
+	t.modules.math=true
+	t.modules.mouse=false
+	t.modules.physics=false
+	t.modules.sound=false
+	t.modules.system=true
+	t.modules.thread=true
+	t.modules.timer=true
+	t.modules.touch=true
+	t.modules.video=false
+	t.modules.window=true
+end
---
+++ main.lua
@@ -0,0 +1,62 @@
+--inicializando variaveis e funcoes
+font=love.graphics.newFont("fonts/PICO-8.ttf", 8)--love.graphics.getFont()
+love.graphics.setFont(font)
+
+function sign(num)
+	return num>0 and 1 or num<0 and -1 or num==0 and 0
+end
+function rotate(x, y, r)
+	local xx=x*math.cos(r)-y*math.sin(r)
+	local yy=x*math.sin(r)+y*math.cos(r)
+	
+	return xx, yy
+end
+
+width, height=512, 256
+local canvas=love.graphics.newCanvas(256, 256)
+w, h=canvas:getDimensions()
+sx, sy=love.graphics.getWidth()/width, love.graphics.getHeight()/height
+love.graphics.setDefaultFilter("nearest")
+
+--carregando arquivos, botoes e mundo
+local button=require "libs/button"
+left=button:new(16, height-64, 32, 32)
+right=button:new(64, height-64, 32, 32)
+jump=button:new(width-64, height-64, 32, 32)
+shurik=button:new(width-64, height-128, 32, 32)
+local world=require "scr/world"
+cam=require "libs/cam"
+anim=require "libs/anim"
+shurikens=require "scr/shurikens"
+mundo=world:new("mapas/2.png")
+
+function love.draw()
+	--telinha
+	love.graphics.setCanvas(canvas)
+	love.graphics.clear(25/255, 25/255, 25/255)
+	
+	cam:func()
+	mundo:draw()
+	cam:reset()
+	love.graphics.rectangle("line", 0, 0, w, h)
+	love.graphics.setCanvas()
+	
+	--desenhando telinha
+	love.graphics.scale(sx, sy)
+	love.graphics.draw(canvas, width/2, height/2, 0, 1, 1, w/2, h/2)
+	--buttons
+	left:draw()
+	right:draw()
+	jump:draw()
+	shurik:draw()
+end
+function love.update(dt)
+	--atualizando o mundo
+	mundo:update(dt)
+	
+	--atualizando os botoes
+	left:update(dt)
+	right:update(dt)
+	jump:update(dt)
+	shurik:update(dt)
+end
